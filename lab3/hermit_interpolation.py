107033c56f5a97d5e0a78e302e9edbf20758e4cb
NiEkOKsU <bartek.kozera00@gmail.com>
2023-03-24 12:50:55 +0100

    sprawko v2

--- lab3/hermit_interpolation.py
+++ lab3/hermit_interpolation.py
@@ -5,23 +5,31 @@ import matplotlib.pyplot as plt
 def fun(x):
     return math.e**(4*math.cos(2*x))
 
-def coef(nodes, values):
+def dfun(x):
+    return -8*math.e**(4*math.cos(2*x))*math.sin(2*x)
+
+def hermie(x, nodes):
     n = len(nodes)
-    a = [values[i] for i in range(n)]
-
-    for j in range(1, n):
-        for i in range(n-1, j-1, -1):
-            a[i] = float(a[i]-a[i-1])/float(nodes[i] - nodes[i - j])
-
-    return np.array(a)
-
-def newton(x, nodes):
-    a = coef(nodes, list(map(fun, nodes)))
-    n = len(a) - 1
-    temp = a[n]
-    for i in range(n - 1, -1, -1):
-        temp = temp * (x - nodes[i]) + a[i]
-    return temp
+    z = []
+    for i in range(n):
+        z.append(nodes[i])
+        z.append(nodes[i])
+    n2 = 2*n
+    matrix = np.zeros((n2, n2))
+    for i in range(n2):
+        for j in range(i+1):
+            if j == 0:
+                matrix[i][j] = fun(z[i])
+            elif j == 1 & i % 2 == 1:
+                matrix[i][j] = dfun(z[i])
+            else:
+                matrix[i][j] = (matrix[i][j-1] - matrix[i-1][j-1]) / (z[i] - z[i-j])
+    result = 0
+    helper = 1
+    for i in range(n2):
+        result += matrix[i][i] * helper
+        helper *= (x - z[i])
+    return result
 
 def equadistant(x0, x1, n):
     result = [x0 + i*(x1-x0)/(n-1) for i in range(n)]
@@ -50,10 +58,10 @@ def main():
     up_bound_x = 3 * math.pi
     points = list(map(lambda x: (low_bound_x + x*(up_bound_x-low_bound_x)/amount), range(amount)))
     fun_values = list(map(fun, points))
-    nodes_num = [2,4,5,7,8,10,13,15,17,20,50]
+    nodes_num = [20]
     for n in nodes_num:
         nodes = equadistant(low_bound_x, up_bound_x, n)
-        interpolated = [newton(points[i], nodes) for i in range(amount)]
+        interpolated = [hermie(points[i], nodes) for i in range(amount)]
         print("Największa różnica: ", max_diff(fun_values, interpolated, amount))
         print("Błąd średniokwadratowy: ", mean_square_error(fun_values, interpolated, amount))
         plt.xlabel('oś x')
