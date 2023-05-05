f17c1feec5d988ad1bc7be955e86e1d89b1eab49
NiEkOKsU <93381337+NiEkOKsU@users.noreply.github.com>
2023-05-05 14:26:43 +0200

    lab 8 start

--- lab6/zad2.py
+++ lab6/zad2.py
@@ -1,88 +1,111 @@
 import numpy as np
-import math
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
+from copy import deepcopy
 
 def save(results):
     df = pd.DataFrame(results)
     print(df)
-    df.to_excel(r'D:\Computation_Methods_for_Science_and_Technology\lab6\test.xlsx')
+    df.to_excel(r'C:\Users\proks\OneDrive\Pulpit\Computation_Methods_for_Science_and_Technology\lab6\res.xlsx')
 
-def chebyshew(x0, x1, n):
-    result = []
-    for i in range(1, n+1, 1):
-        result.append(1/2*(x0 + x1) + 1/2*(x1 - x0)*math.cos((2*i - 1)*math.pi/(2*n)))
-    return result
+class TrigonometricApproximation:
+    def __init__(self, X, Y, n, m, start, stop):
+        if m > np.floor((n-1)/2):
+            raise Exception("m cannot be greater than floor of (n-1)/2")
+        self.X = X
+        self.Y = Y
+        self.n = n
+        self.m = m
+        self.start = start
+        self.stop = stop
+        self.A = np.zeros(self.n)
+        self.B = np.zeros(self.n)
+        self.scale_to_2pi()
+        self.compute_A_and_B()
+        self.scale_from_2pi()
 
+    def scale_to_2pi(self):
+        range_length = self.stop - self.start
+        for i in range(len(self.X)):
+            self.X[i] /= range_length
+            self.X[i] *= 2 * np.pi
+            self.X[i] += -np.pi - (2 * np.pi * self.start / range_length)
 
-def equadistant(x0, x1, n):
-    result = []
-    for i in range(n):
-        result.append(x0 + i*(x1-x0)/(n-1))
-    return result
+    def compute_A_and_B(self):
+        for i in range(self.n):
+            ai = sum(self.Y[j] * np.cos(i * self.X[j]) for j in range(self.n))
+            bi = sum(self.Y[j] * np.sin(i * self.X[j]) for j in range(self.n))
+            self.A[i] = 2 * ai / self.n
+            self.B[i] = 2 * bi / self.n
 
-def max_diff(fval, intrval, n):
-    max_df = 0
-    for i in range(n):
-        max_df = max(abs(fval[i] - intrval[i]), max_df)
-    return max_df
+    def scale_from_2pi(self):
+        range_length = self.stop - self.start
+        for i in range(len(self.X)):
+            self.X[i] -= -np.pi - (2 * np.pi * self.start / range_length)
+            self.X[i] /= 2 * np.pi
+            self.X[i] *= range_length
 
+    def scale_point_to_2pi(self, x):
+        range_length = self.stop - self.start
+        x /= range_length
+        x *= 2 * np.pi
+        x += -np.pi - (2 * np.pi * self.start / range_length)
+        return x
 
-def mean_square_error(fval, intrval, n):
-    error = 0
-    for i in range(n):
-        error += (fval[i] - intrval[i]) ** 2
-    error = math.sqrt(error) / n
-    return error
-
+    def approximate(self, X):
+        points = []
+        for x in X:
+            cp_x = deepcopy(x)
+            cp_x = self.scale_point_to_2pi(cp_x)
+            approximated_x = 1 / 2 * self.A[0] + sum(self.A[j] * np.cos(j * cp_x) + self.B[j] * np.sin(j * cp_x)
+                                                     for j in range(1, self.m + 1))
+            points.append(approximated_x)
+        return points
 
 def f(x):
     return np.e**(4*np.cos(2*x))
 
-def get_trig_coeff(m, k, nodes):
-    akl = []
-    bkl = []
-    n = len(nodes)
-    for i in range(n):
-        akl.append(f(nodes[i]*2) * np.cos(k*nodes[i]))
-        bkl.append(f(nodes[i]*2) * np.sin(k*nodes[i]))
-    return (sum(akl) / (n/2)), (sum(bkl) / (n/2))
+def max_error(Y1, Y2):
+    return max([abs(Y1[i] - Y2[i]) for i in range(len(Y1))])
 
+def mean_square_error(Y1, Y2):
+    return np.sqrt(sum([(Y1[i] - Y2[i])**2 for i in range(len(Y1))]))/1000
 
-def approximate_trig(x, nodes, m):  # N is for degree of the approximation
-    a0 = get_trig_coeff(m, 0, nodes)[0]
-    res = 0
-    for k in range(1, m):
-        ak, bk = get_trig_coeff(m, k, nodes)
-        res += 1/2 * ak * np.cos(k * x)
-        res += 1/2 * bk * np.sin(k * x)
-    res += a0 / 2
-    return res
-
-
+def visualize(x, y, start, stop, n, m, function):
+    plt.figure(figsize=(8, 6))
+    plt.scatter(x, y, label="Węzły", color="red")
+    X = np.arange(start, stop + 0.01, 0.01)
+    plt.plot(X, f(X), label="Funckja aproksymowana", color="blue")
+    plt.plot(X, function(X), label="Wielomian aproksymujący", color="grey")
+    plt.title(f"Aproksymacji trygonometryczna dla {n} węzłów i m={m}")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.legend()
+    plt.show()
+    
+def trig_approximation(start, stop, n, m):
+    X = np.linspace(start, stop, n)
+    Y = f(X)
+    trigonometric_approximation = TrigonometricApproximation(X, Y, n, m, start, stop)
+    visualize(X, Y, start, stop, n, m, trigonometric_approximation.approximate)
+    
 def main():
-    wezly = [15]
+    wezly = [5, 10, 15, 30,50, 100]
     res = [['Liczba węzłów', 'stopien wielomanu','blad max', 'mse']]
-    amount = 10000
-    x0 = -math.pi
-    x1 = 3 * math.pi
-    numbers = range(amount)
+    amount = 1000
+    x0 = -np.pi
+    x1 = 3 * np.pi
+    total_X = np.linspace(x0, x1, amount)
+    func_val = f(total_X)
     for n in wezly:
-        for degree in range(10,13):
-            if n<=degree:
+        for f_num in [2,3,4,5,6,7,8,9,10,15,20,25,30]:
+            if f_num > np.floor((n-1)/2):
                 break
-            points = list(map(lambda x: (x0 + x*(x1-x0)/amount), numbers))
-            values = list(map(f, points))
-            nodes = equadistant(x0, x1, n)
-            regressed = [approximate_trig(points[i], nodes, degree) for i in range(amount)]
-            res.append([n, degree, max_diff(regressed, values, amount), mean_square_error(regressed, values, amount)])
-            print(n, degree)
-            plt.xlabel('oś X')
-            plt.ylabel('oś Y')
-            plt.title('Aproksymacja')
-            plt.plot(points, values, 'b-', points, regressed, 'grey',
-                    nodes, list(map(f, nodes)), 'r.', markersize=10)
-            plt.show()
-    save(res)
-
+            X = np.linspace(x0, x1, n)
+            Y = f(X)
+            trigonometric_approximation = TrigonometricApproximation(X, Y, n, f_num, x0, x1)
+            trig_appr_result = trigonometric_approximation.approximate(total_X)
+            res.append([n, f_num, max_error(trig_appr_result, func_val), mean_square_error(trig_appr_result, func_val)])
+            visualize(X, Y, x0, x1, n, f_num, trigonometric_approximation.approximate)
+    #save(res) żeby zapisać wyniki i je wyświtlić należy odkomentowaćtą linijkę
 main()
---
+++ lab8/rownania_nieliniowe.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+#pd.options.display.float_format = "{:,.12f}".format
+
+def func1(x):
+    return x**14 + x**13
+
+def der_func1(x):
+    return 14*x**13 + 13*x**12
+
+def newtons_method(func, der, x_0, epsilon, max_iter, stop_condition):
+    x_n = x_0
+    for n in range(max_iter):
+        f_xn = func(x_n)
+        der_fxn = der(x_n)
+        if der_fxn == 0:
+            # Zero derivative. No solution found.
+            return None, None
+        if stop_condition == "abs" and abs(f_xn) < epsilon:
+            # Found solution
+            return x_n, n
+        elif stop_condition == "points" and abs(f_xn / der_fxn) < epsilon:
+            # Found solution
+            return x_n, n
+        x_n -= f_xn / der_fxn
+    # Exceeded maximum number of iterations. No solution found.
+    return None, np.inf
+
+def secant(func, x_1, x_2, epsilon, max_iter, stop_condition):
+    for n in range(max_iter):
+        if func(x_1) == func(x_2):
+            # Divided by zero
+            return None, None
+        x_1, x_2 = x_2, x_2 - (x_2 - x_1) * func(x_2) / (func(x_2) - func(x_1))
+        if stop_condition == "abs" and abs(func(x_2)) < epsilon:
+            # Found solution
+            return x_2, n
+        elif stop_condition == "points" and abs(x_1 - x_2) < epsilon:
+            # Found solution
+            return x_2, n
+    # Exceeded maximum number of iterations. No solution found.
+    return None, np.inf
+
+def create_dataframe(method_name, epsilon, max_iter, stop_condition):
+    X = np.arange(-1, 1 + 0.1, 0.1)
+    result = []
+    for x_0 in X:
+        if method_name == "newton":
+            x, n = newtons_method(func1, der_func1, x_0, np.e**(-20), max_iter, stop_condition)
+            result += [x, n, x_0]
+        elif method_name == "secant":
+                x, n = secant(func1, x_0, 1, np.e**(-20), 100, "points")
+                result += [x, n, x_0]
+                x, n = secant(func1, -1, x_0, np.e**(-20), 100, "points")
+                result += [x, n, x_0]
+    df = pd.DataFrame(data={"x value": result[::3],
+                            "num of iterations": result[1::3],
+                            "point": result[2::3]})
+    return df
+
+def plot():
+    X = np.arange(-1.4, 0.6+0.01, 0.01)
+    plt.plot(X, func1(X), label = "Funkcja")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.grid()
+    plt.show()
+
+plot()
