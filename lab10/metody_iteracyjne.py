2ba222690f5784474ad3e93f30cad4bf6ebe17f6
NiEkOKsU <bartek.kozera00@gmail.com>
2023-06-23 14:39:05 +0200

    final commit

--- lab10/metody_iteracyjne.py
+++ lab10/metody_iteracyjne.py
@@ -3,15 +3,16 @@ import pandas as pd
 from time import perf_counter
 
 def save(results):
-    df = pd.DataFrame(results)
-    print(df)
-    path = r'C:\Users\proks\OneDrive\Pulpit\Computation_Methods_for_Science_and_Technology\lab10\res2.xlsx'
-    df.to_excel(path)
+    print(results)
+    path = r'D:\Computation_Methods_for_Science_and_Technology\lab10\resspektral.xlsx'
+    results.to_excel(path)
 
 def jacobi_method(A, b, stop_cond, epsilon, max_iters):
     D = np.diag(A)
     R = A - np.diagflat(D)
     X = np.zeros_like(b)
+    for i in range(len(X)):
+        X[i] = 100
     iters = 0
     for _ in range(max_iters):
         X_new = (b - (R @ X)) / D
@@ -28,16 +29,16 @@ def matrix_create(A, n, k, m):
         for j in range(1, n+1):
             if i == j:
                 A[i-1][i-1] = k
-            elif i == j-1:
+            elif i-1 == j:
                 A[i-1][j-1] = m/i
             elif j > i:
-                A[i-1][j-1] = (-1)**j*(m/j)
+                A[i-1][j-1] = (-1)**j * m/j
     return A
 
 def exercise_1(numbers, epsilon, k, m, max_iters):
     result = []
     for n in numbers:
-        A = np.array([[0 for _ in range(n)] for _ in range(n)])
+        A = np.zeros((n, n))
         A = matrix_create(A, n, k, m)
         X_vec = np.array([1 if i % 2 == 0 else -1 for i in range(n)])
         b = A @ X_vec
@@ -65,7 +66,28 @@ def exercise_1(numbers, epsilon, k, m, max_iters):
                             "2nd condition norm": result[5::6]})
     return df
 
-numbers = [3, 4, 5, 7, 10, 12, 15, 20, 30, 50, 70, 100, 150, 200, 300, 500]
-epsilon = 0.00001
-df_1 = exercise_1(numbers, epsilon, 7, 1, 2000)
+def spectral_radius(A):
+    D = np.diag(A)
+    R = A - np.diagflat(D)
+    S = R / D
+    eigvals = np.linalg.eigvals(S)
+    return max(abs(i) for i in eigvals)
+
+def exercise_2(numbers, k, m):
+    result = []
+    for n in numbers:
+        A = np.zeros((n, n)).astype(np.float64)
+        A = matrix_create(A, n, k, m)
+        spec_rad = spectral_radius(A)
+        condition = True
+        if spec_rad >= 1:
+            condition = False
+        result += [spec_rad, condition]
+    df = pd.DataFrame(data={"n": numbers,
+                            "spectral radius": result[::2],
+                            "condition": result[1::2]})
+    return df
+
+numbers = [3, 4, 5, 7, 10, 12, 15, 20, 30, 50, 70, 100, 150, 200, 300, 500, 1000]
+df_1 = exercise_2(numbers, 7, 1)
 save(df_1)
